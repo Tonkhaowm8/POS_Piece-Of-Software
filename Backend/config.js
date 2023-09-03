1fe711047e996fab3405ac179b762c5a363a1b94
Tonkhaowm8 <Tonkhaowm8@gmail.com>
2023-09-03 17:44:04 +0700

    fix config error

--- Backend/config.js
+++ Backend/config.js
@@ -1,15 +1,23 @@
-const credit = require('credentials.json')
+const config = require("./credentials.json")
+const fs = require('fs')
+let cred = "hi"
 
-console.log("hello")
-console.log(credit(ACCESS_KEY_ID))
+try{
+  const jsonString = fs.readFileSync("./credentials.json");
+  cred = JSON.parse(jsonString);
+} catch (err) {
+  console.log(err);
+  return;
+}
+
 module.exports = {
     aws_table_name: 'dynamodb-test',
     aws_local_config: {
       //Provide details for local configuration
     },
     aws_remote_config: {
-      accessKeyId: credit(ACCESS_KEY_ID),
-      secretAccessKey: 'SECRET_ACCESS_KEY',
+      accessKeyId: (cred.ACCESS_KEY_ID),
+      secretAccessKey: (cred.SECRET_ACCESS_KEY),
       region: 'us-east-1',
     }
 };
