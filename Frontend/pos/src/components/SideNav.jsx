8ac8af16eef7a3998bfec750b044c4e7cb2f6d49
785putt <mr.putty02@gmail.com>
2023-09-06 23:18:33 +0700

    install rsuite node into

--- Frontend/pos/src/components/SideNav.jsx
+++ Frontend/pos/src/components/SideNav.jsx
@@ -3,21 +3,61 @@ import styled from 'styled-components';
 import SideNav, {Toggle, NavItem, NavIcon, NavText} from '@trendmicro/react-sidenav';
 import { useNavigate } from "react-router-dom";
 import "@trendmicro/react-sidenav/dist/react-sidenav.css";  
-import logo_2 from './pos/public/logo_ 2.png';
+import SidebarItems from "./SideNavItems";
+// import logo_2 from './pos/public/logo_ 2.png';
 
+// const SidebarParent = styled.div`
+//   background: #c34a36;
+//   width: 250px;
+//   height: 100vh;
+// `;
+
+// const SidebarItem = styled.div`
+//   padding: 16px 24px;
+//   transition: all 0.25s ease-in-out;
+//   //Change the background color if 'active' prop is received
+//   background: ${props => props.active ? "#b15b00" : ""};
+//   margin: 4px 12px;
+//   border-radius: 4px;
+
+//   p {
+//     color: white;
+//     font-weight: bold;
+//     text-decoration: none;
+//   }
+
+//   &:hover {
+//     cursor:pointer;
+//   }
+
+//   &:hover:not(:first-child) {
+//     background: #c34a36;
+//   }
+// `;
 
 function Nav() {
     const navigate = useNavigate();
     return(
 
         <div className="top_navbar">
+            {/* <SidebarParent>
+                {
+                    SidebarItems.map(item=> (
+                        <SidebarItem key={item.name}>
+                            <p>{item.name}</p>
+                        </SidebarItem>
+                    ))
+                }
+            </SidebarParent> */}
+
+                {/* Sidebar using SideNav components: hard to adjust*/}
                 <SideNav
-            onSelect={selected => {
-                console.log(selected)
-                navigate('/'+selected)
-            }}
-            className = 'mysidenav'
-            >
+                onSelect={selected => {
+                    console.log(selected)
+                    navigate('/'+selected)
+                }}
+                className = 'mysidenav'
+                >
                 <div class="logo">Stock</div>
                 <SideNav.Toggle />
                 <SideNav.Nav defaultSelected="Stock">
---
+++ Frontend/pos/src/components/SideNavItems.jsx
@@ -0,0 +1,20 @@
+const SidebarItems = [
+    {
+        name: "THE ITALIAN JOB",
+        route: '/'
+    },
+    {
+        name: "Dashboard",
+        route: '/dashboard',
+    },
+    {
+        name: "Stock",
+        route: '/Stock'
+    },
+    {
+        name: "Logout",
+        route: '/Logout'
+    },
+];
+
+export default SidebarItems;
