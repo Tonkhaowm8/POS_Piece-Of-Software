c5194c2a9a29d59d4aa69b0fea108ff91a069b07
785putt <mr.putty02@gmail.com>
2023-09-10 23:34:05 +0700

    Add in Login page (4th, pls do this page)

---
+++ Frontend/pos/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "../components/SideNav";
+import Nav from "../components/SideNav";
+
+function Login(props) {
+    return(
+        <div className="page">
+            <h2>Login</h2>
+        </div>
+        // โฟรท์,    เปลี่ยน HTML elements ต่างๆในนี้ให้มีปุ่มต่างๆ เเบบที่มึงเห็นในเว็บหน้าล็อกอินตาม figma
+    );
+}
+export default Login;
