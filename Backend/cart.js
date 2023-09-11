85904e97c91fd4b2e06a9e61e32a1c8d2524554a
Millyaa-11 <chalitaes@gmail.com>
2023-09-12 02:52:21 +0700

    finish cart WARNING DELETE CACHE!

--- Backend/cart.js
+++ Backend/cart.js
@@ -2,20 +2,15 @@ const cartQueue = [];
 let totalCost = 0; // Initialize the total cost variable
 
 // Function for adding items to the shopping cart queue
-const addToCartQueue = async (userId, itemId, quantity = 1) => {
+const addToCartQueue = async (userId, itemId, price, quantity = 1) => {
     try {
-        // Fetch the item data and price from the route
-        const response = await fetch(`/api/item/${itemId}`);
-        const itemData = await response.json();
-
-        // Calculate the total cost based on the fetched price
-        const price = itemData.data.price;
         const cartItem = { userId, itemId, quantity, price };
         cartQueue.push(cartItem);
 
         // Calculate and update the total cost when items are added
         totalCost += price * quantity;
-
+        console.log(`Adding cart item for user ${cartItem.userId}`);
+        console.log(`Item ID: ${cartItem.itemId}, Quantity: ${cartItem.quantity}, Total: ${totalCost}`);
         return { success: true };
     } catch (error) {
         return { success: false, error: error.message };
@@ -23,26 +18,43 @@ const addToCartQueue = async (userId, itemId, quantity = 1) => {
 };
 
 // Function for removing items from the shopping cart queue by userId and itemId
-const removeFromCartQueue = (userId, itemId, price) => {
-    const updatedCartQueue = cartQueue.filter((cartItem) => {
-        if (cartItem.userId === userId && cartItem.itemId === itemId) {
-            // Calculate and subtract the cost of the item being removed
-            totalCost -= price * cartItem.quantity;
-            return false; // Exclude the item being removed from the updated queue
+const removeFromCartQueue = (userId, itemId) => {
+    let found = false; // Track whether an item has been removed
+
+    const updatedCartQueue = cartQueue.map((cartItem) => {
+        if (!found && cartItem.userId === userId && cartItem.itemId === itemId) {
+            // Calculate and subtract the cost of one item being removed
+            totalCost -= cartItem.price;
+            // Decrease the quantity by 1
+            cartItem.quantity -= 1;
+            found = true; // Set found to true so that only one quantity of the item is removed
+            console.log(`Removing cart item for user ${cartItem.userId}`);
+            console.log(`Item ID: ${cartItem.itemId}, Quantity: ${cartItem.quantity}, Total: ${totalCost}`);
         }
-        return true; // Include other items in the updated queue
+        return cartItem; // Return the modified or unmodified item
     });
+
+    // Remove items with a quantity of 0 (or less)
+    const filteredCartQueue = updatedCartQueue.filter((cartItem) => cartItem.quantity > 0);
 
     // Update the cart queue with the filtered items
     cartQueue.length = 0; // Clear the current cart queue
-    updatedCartQueue.forEach((item) => {
-        cartQueue.push(item); // Push the updated items back into the queue
-    });
+    cartQueue.push(...filteredCartQueue); // Push the updated items back into the queue
 
-    return { success: true };
+    return { success: found }; // Return whether an item quantity was removed
 };
 
-// Function for processing the shopping cart queue
+
+// Function for displaying all items in the shopping cart
+const displayCart = () => {
+    console.log("Items in the cart:");
+    cartQueue.forEach((cartItem) => {
+        console.log(`User ID: ${cartItem.userId}, Item ID: ${cartItem.itemId}, Quantity: ${cartItem.quantity}`);
+    });
+    console.log(`Total Cost: ${getTotalCost()}`);
+};
+
+// Function for processing the shopping cart queue (unchanged)
 const processCartQueue = async () => {
     while (cartQueue.length > 0) {
         const cartItem = cartQueue.shift();
@@ -51,15 +63,6 @@ const processCartQueue = async () => {
         // based on the cartItem data.
         console.log(`Processing cart item for user ${cartItem.userId}`);
         console.log(`Item ID: ${cartItem.itemId}, Quantity: ${cartItem.quantity}`);
-
-        // You can also fetch additional item details if needed from the route or database.
-        try {
-            const response = await fetch(`/api/item/${cartItem.itemId}`);
-            const itemData = await response.json();
-            console.log(`Item Name: ${itemData.data.name}`);
-        } catch (error) {
-            console.error(`Error fetching item details: ${error.message}`);
-        }
     }
 };
 
@@ -67,8 +70,7 @@ const processCartQueue = async () => {
 const getTotalCost = () => totalCost;
 
 // Example usage:
-addToCartQueue(1, 101, 2); // Adding an item with itemId 101
-addToCartQueue(1, 102, 1); // Adding an item with itemId 102
-removeFromCartQueue(1, 101, 10); // Removing an item with itemId 101
-await processCartQueue(); // Process the remaining items in the cart (use await for asynchronous fetch)
-console.log(`Total Cost: ${getTotalCost()}`); // Get and print the total cost
+addToCartQueue(1, 101, 10, 2); // Adding an item with itemId 101
+addToCartQueue(1, 102, 10, 1); // Adding an item with itemId 102
+removeFromCartQueue(1, 101); // Removing an item with itemId 101
+displayCart(); // Display all items in the cart and the total value
