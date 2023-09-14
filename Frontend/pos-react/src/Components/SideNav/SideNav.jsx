41e1e8fe278c7f94a2714e6f58ca313729252a2b
785putt <mr.putty02@gmail.com>
2023-09-14 23:51:05 +0700

    sidebar nav work as intended fully

--- Frontend/pos-react/src/Components/SideNav/SideNav.jsx
+++ Frontend/pos-react/src/Components/SideNav/SideNav.jsx
@@ -32,36 +32,11 @@ function Nav({show, children}) {
                         </li>
                     ))}
                 </ul>
-                <main className="contentt">{children}</main>    
+                <main className={show ? "contentt active" : "contentt"}>{children}</main>    
             </div>    
             {/* <main className="contentt">{children}</main>                */}
         </div>
-        // {/* <SideNav
-        //             onSelect={selected => {
-        //                 console.log(selected);
-        //                 navigate('/' + selected);
-        //             }}
-        //             className='mysidenav'
-        //         >
-        //             <div className="logo">
-        //                 <img className="logo" src={require("C:/Users/mrput/Documents/VSProject/POS_Piece-Of-Software/Frontend/pos/src/components/logo_ 2.png")} alt="Stock | Product" style={{ height: 'auto', width: 'auto' }} />
-        //             </div>
-        //             <SideNav.Toggle onClick={handleToggle} />
-        //             <SideNav.Nav defaultSelected="Stock">
-        //                 <NavItem eventKey="Dashboard" style={{ margin: '45px 0px 0px' }}>
-        //                     <NavIcon><i class="fa-solid fa-table-columns"></i></NavIcon>
-        //                     <NavText>Dashboard</NavText>
-        //                 </NavItem>
-        //                 <NavItem eventKey="Stock">
-        //                     <NavIcon><i class="fa-solid fa-database"></i></NavIcon>
-        //                     <NavText>Stock</NavText>
-        //                 </NavItem>
-        //                 <NavItem eventKey="Logout" style={{ position: 'absolute', display: 'flex', bottom: 0 }}>
-        //                     <NavIcon><i class="fa-solid fa-arrow-right-from-bracket"></i></NavIcon>
-        //                     <NavText>Log out</NavText>
-        //                 </NavItem>
-        //             </SideNav.Nav>
-        //         </SideNav> */}
+        
     );
 }
 
