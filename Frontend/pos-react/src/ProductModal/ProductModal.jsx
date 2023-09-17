6e5821b8b4d404bdabc00d59905d54ecd447fd5a
785putt <mr.putty02@gmail.com>
2023-09-17 23:09:40 +0700

    modal draggable

--- Frontend/pos-react/src/ProductModal/ProductModal.jsx
+++ Frontend/pos-react/src/ProductModal/ProductModal.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Draggable from "react-draggable"; // Import Draggable from the react-draggable library
 import './ProductModal.css';
 
 function ProductModal({ isOpen, onClose }) {
@@ -17,50 +18,52 @@ function ProductModal({ isOpen, onClose }) {
 
   return (
     isOpen && (
-      <div className="modal-wrapper">
-        <div className="modal">
-          <h2>Add Product</h2>
-          <form onSubmit={handleSubmit}>
-            <label>
-              Product Name:
-              <input
-                type="text"
-                value={productName}
-                onChange={(e) => setProductName(e.target.value)}
-              />
-            </label>
-            <label>
-              Stock:
-              <input
-                type="text"
-                value={stock}
-                onChange={(e) => setStock(e.target.value)}
-              />
-            </label>
-            <label>
-              Price:
-              <input
-                type="text"
-                value={price}
-                onChange={(e) => setPrice(e.target.value)}
-              />
-            </label>
-            <label>
-              Id:
-              <input
-                type="number"
-                value={id}
-                onChange={(e) => setId(e.target.value)}
-              />
-            </label>
-            <button type="submit">Save</button>
-          </form>
-          <button className="close-button" onClick={onClose}>
-            Close
-          </button>
+      <Draggable bounds={{ top: 0, left: 0, right: 200, bottom: 150 }}>
+         <div className="modal-wrapper">
+            <div className="modal">
+            <h2>Add Product</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                Product Name:
+                <input
+                    type="text"
+                    value={productName}
+                    onChange={(e) => setProductName(e.target.value)}
+                />
+                </label>
+                <label>
+                Stock:
+                <input
+                    type="text"
+                    value={stock}
+                    onChange={(e) => setStock(e.target.value)}
+                />
+                </label>
+                <label>
+                Price:
+                <input
+                    type="text"
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                />
+                </label>
+                <label>
+                Id:
+                <input
+                    type="number"
+                    value={id}
+                    onChange={(e) => setId(e.target.value)}
+                />
+                </label>
+                <button type="submit">Save</button>
+            </form>
+            <button className="close-button" onClick={onClose}>
+                Close
+            </button>
+            </div>
+            <div className="modal-backdrop" onClick={onClose}></div>
         </div>
-        <div className="modal-backdrop" onClick={onClose}></div>
-      </div>
+      </Draggable>
     )
   );
 }
